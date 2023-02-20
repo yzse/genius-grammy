0b463efd3c571fd8cc0f7a7b3cc881a047837bf3
shaney <wsze14@bloomberg.net>
2023-02-20 21:16:45 +0100

    upload time graph code

---
+++ make_time_graph.py
@@ -0,0 +1,25 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+# read data and filter outliers
+df = pd.read_csv('rescale_df.csv')
+mean_gloom = df['gloom_index'].mean()
+std_gloom = df['gloom_index'].std()
+df = df[(df['gloom_index'] > mean_gloom - 4 * std_gloom) & (df['gloom_index'] < mean_gloom + 4 * std_gloom)]
+
+#%%
+sns.set_style('whitegrid')
+sns.lmplot(x='year', y='gloom_index', hue='winner', data=df, scatter_kws={"alpha":0.5},
+           height=5, aspect=1.5, legend=False);
+plt.legend(title='Status', labels=['Nominee', 'Winner'], loc='upper right')
+plt.title('Gloom Index by Year and Winner')
+plt.xlabel('Year')
+plt.ylabel('Gloom Index')
+
+plt.savefig('gloom_index_time_series.jpeg', dpi=300, bbox_inches='tight')
+
+# %%
