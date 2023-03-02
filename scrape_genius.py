68ad184f304ca1e6e482f7b3f948c8e33dfab562
Luis Quiñones <luisquinonespr@Luiss-Air.home>
2023-03-03 00:11:12 +0100

    fixing models and preprocessing

--- scrape_genius.py
+++ scrape_genius.py
@@ -1,22 +1,19 @@
 import lyricsgenius
-import pandas as pd
+import pandas 
 
-def get_lyrics_from_songs(df, access_token):
+def get_lyrics(df, artist_column, song_column, access_token):
     genius = lyricsgenius.Genius(access_token, remove_section_headers=True, timeout=40)
-    lyrics = []
-    song_names = []
-
-    # Parse songs
-    for index, row in df.iterrows():
-        title = row['title']
-        artist = row.get('artist')
-        if pd.notna(artist):
-            song = genius.search_song(title, artist)
-        else:
-            song = genius.search_song(title)
-        if song is not None:
-            song_names.append(title)
-            lyrics.append(song.lyrics)
-
-    df['lyrics'] = lyrics
-    df.to_csv('genius_grammy.csv')
+    df['song_lyrics'] = None
+    not_found = []
+    for i, row in df.iterrows():
+        try:
+            song = genius.search_song(row[song_column], row[artist_column])
+            lyrics = song.lyrics
+            df.at[i, 'song_lyrics'] = lyrics
+        except:
+            not_found.append(f'{row[artist_column]} - {row[song_column]}')
+            df.at[i, 'song_lyrics'] = None
+        finally:
+            pass
+    df['song_lyrics'] = df['song_lyrics'].str.replace('\n', ' ')
+    df.to_csv('genius_grammy.csv', index=False)
